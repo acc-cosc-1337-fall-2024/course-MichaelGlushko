bdc5941b9d8eb19d854c519ef6396e503f46a9a1
MichaelGlushko <157078135+MichaelGlushko@users.noreply.github.com>
2024-02-18 04:00:33 +0000

    Finished 03 homework

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,64 @@
 //write include statement for decisions header
+#include"decisions.h"
+#include<iostream>
 
+using std::cout;
 
 //Write code for function(s) code here
+int get_letter_grade_using_if(int grade)
+{
+    char letter_grade;
+
+    if(grade > 89 && grade <= 100)
+    {
+        letter_grade = 'A';
+    }
+    else if(grade > 79 && grade < 90)
+    {
+        letter_grade = 'B';
+    }
+    else if(grade > 69 && grade < 80)
+    {
+        letter_grade = 'C';
+    }
+    else if(grade > 59 && grade < 70)
+    {
+        letter_grade = 'D';
+    }
+    else if(grade >= 0 && grade < 60)
+    {
+        letter_grade = 'F';
+    }
+    else
+    {
+        cout<<"Not within a valid range";
+    }
+    return letter_grade;
+}
+
+int get_letter_grade_using_switch(int grade)
+{
+    char letter_grade;
+
+    switch(grade)
+    {
+        case 0 ... 59:
+            letter_grade = 'F';
+            break;
+        case 60 ... 69:
+            letter_grade = 'D';
+            break;
+        case 70 ... 79:
+            letter_grade = 'C';
+            break;
+        case 80 ... 89:
+            letter_grade = 'B';
+            break;
+        case 90 ... 100:
+            letter_grade = 'A';
+            break;
+        default:
+            cout<<"Not within a valid range.";   
+    }
+    return letter_grade;
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,46 @@
 //write include statements
+#include<iostream>
+#include"decisions.h"
+
+using std::cin;
+using std::cout;
 
 int main() 
 {
+	int menu_input;
+	int grade;
+	char letter_grade;
+	
+	cout<<"\n";
+	cout<<"     MAIN MENU\n\n1-Letter grade using if\n2-Letter grade using switch\n3-Exit\n";
+	cin>>menu_input;
+	
+	if(menu_input == 1)
+	{
+		cout<<"Please enter a number between 0 and 100: ";
+		cin>>grade;
+
+		letter_grade = get_letter_grade_using_if(grade);
+		cout<<letter_grade<<"\n";
+	}
+	else if(menu_input == 2)
+	{
+		cout<<"Please enter a number between 0 and 100: ";
+		cin>>grade;
+
+		letter_grade = get_letter_grade_using_switch(grade);
+		cout<<letter_grade<<"\n";
+	}
+	else if(menu_input == 3)
+	{
+		cout<<"Exited Program\n";
+	}
+	else 
+	{
+		cout<<"Invalid Input\n";
+	}
+	
+
+
 	return 0;
 }
