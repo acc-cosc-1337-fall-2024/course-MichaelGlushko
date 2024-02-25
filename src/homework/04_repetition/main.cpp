4615c4acfc2861baeeebefcc0d6f5e77e5e24b11
MichaelGlushko <157078135+MichaelGlushko@users.noreply.github.com>
2024-02-25 08:57:13 +0000

    Finished 04 repetition HW

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,8 +1,46 @@
 //write include statements
+#include "repetition.h"
+#include <iostream>
 
+using std::cin;
+using std::cout;
 //write using statements
 
 int main() 
 {
+	int choice;
+	char confirmation;
+	int num1;
+	int num2;
+
+	do
+	{
+		cout<<"\n\n1-Factorial\n2-Greatest Common Divisor\n3-Exit\n";
+		cin>>choice;
+
+		if (choice == 1) 
+		{
+			cout<<"\nInput a number: ";
+			cin>>num1;
+
+			cout<<"\n"<<factorial(num1);
+		}
+		else if (choice == 2)
+		{
+			cout<<"\nInput number 1: ";
+			cin>>num1;
+			cout<<"\nInput number 2: ";
+			cin>>num2;
+
+			cout<<"\n"<<gcd(num1, num2);
+		}
+		else if (choice == 3)
+		{
+			cout<<"\nAre you sure you want to exit? (Y/N): ";
+			cin>>confirmation;
+		}
+	} while (confirmation != 'Y');
+	
+
 	return 0;
 }
--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,36 @@
 //add include statements
+#include "repetition.h"
+#include<iostream>
 
 //add function(s) code here
+
+int factorial(int num) 
+{
+    int sum = 1;
+
+    for (int i = 1; i <= num; i++) 
+    {
+        sum = i * sum;
+    }
+    
+    return sum;
+}
+
+int gcd(int num1, int num2)
+{
+    int t;
+    while (num1 != num2)
+    {
+        if (num1 < num2)
+        {
+            t = num1;
+            num1 = num2;
+            num2 = t;
+        }
+        else if (num1 > num2)
+        {
+            num1 -= num2;
+        }
+    }
+    return num1;
+}
